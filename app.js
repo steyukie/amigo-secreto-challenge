45138d8e18234dfb5bdcdcb8078e9ba11e976d7c
steyukie <stefanyukie@gmail.com>
2025-01-31 22:36:19 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,9 @@
+let lista = [];
+let listaAmigos = document.getElementById("listaAmigos");
+let nomeInput = document.getElementById("amigo");
+let resultado = document.getElementById("resultado");
+
+function adicionarAmigo() {
+    let nomeAmigo = nomeInput.value;
+    console.log(amigo);
+}
